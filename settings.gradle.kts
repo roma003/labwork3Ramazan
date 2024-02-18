f1ea672a783f5751b3f0ef30d86aefe723a89e67
roma003 <ramazan020621@gmail.com>
2024-02-18 16:21:31 +0600

    lab3

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ramazan_Amanzhol_sis2125_lab3"
+include(":app")
+ 
